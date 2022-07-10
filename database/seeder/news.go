edc2e1ee5d0894abffe8bb85e5b74b6167a08982
siffiyan1 <siffiyanassauri1@gmail.com>
2022-07-10 12:24:14 +0700

    add form model news

--- database/seeder/news.go
+++ database/seeder/news.go
@@ -12,7 +12,7 @@ func newsTableSeeder(conn *gorm.DB) {
 	var news = []model.News{}
 
 	for i := 0; i < 15; i++ {
-		news = append(news, model.News{Title: "News", Description: "News Descriptiom", Image: "-", IsActive: 1})
+		news = append(news, model.News{Title: "News", Description: "News Description", Image: "-", IsActive: 1})
 	}
 
 	if err := conn.Create(&news).Error; err != nil {
--- internal/dto/news.go
+++ internal/dto/news.go
@@ -1,10 +1,10 @@
 package dto
 
 type CreateNewsRequest struct {
-	Title       string `json:"title" validate:"required"`
-	Image       string `json:"image" validate:"required"`
-	Description string `json:"description"`
-	IsActive    int    `json:"is_active" validate:"required"`
+	Title       string `json:"title" form:"title" validate:"required"`
+	Image       string `json:"image" form:"image" validate:"required"`
+	Description string `json:"description" form:"description"`
+	IsActive    int    `json:"is_active" form:"is_active" validate:"required"`
 }
 
 type UpdateNewsRequest struct {
