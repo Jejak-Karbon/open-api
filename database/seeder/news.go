7fc8e1c815eba28562a130fc10fc1a2f6290329a
siffiyan1 <siffiyanassauri1@gmail.com>
2022-07-10 21:20:37 +0700

    done connect rds

--- database/seeder/news.go
+++ database/seeder/news.go
@@ -11,7 +11,7 @@ func newsTableSeeder(conn *gorm.DB) {
 
 	var news = []model.News{}
 
-	for i := 0; i < 15; i++ {
+	for i := 0; i < 2; i++ {
 		news = append(news, model.News{Title: "News", Description: "News Description", Image: "-", IsActive: 1})
 	}
 
