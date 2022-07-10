9c466ad8f0682bf12f5e981dfe1b24df36ce5115
siffiyan1 <siffiyanassauri1@gmail.com>
2022-07-10 12:37:03 +0700

    add static file aws

--- internal/app/news/service.go
+++ internal/app/news/service.go
@@ -1,14 +1,22 @@
 package news
 
 import (
+	"os"
+	"bytes"
+	"io/ioutil"
+	"log"
 	"context"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"github.com/born2ngopi/alterra/basic-echo-mvc/internal/dto"
 	"github.com/born2ngopi/alterra/basic-echo-mvc/internal/factory"
 	"github.com/born2ngopi/alterra/basic-echo-mvc/internal/model"
 	"github.com/born2ngopi/alterra/basic-echo-mvc/internal/repository"
 	"github.com/born2ngopi/alterra/basic-echo-mvc/pkg/constant"
 	res "github.com/born2ngopi/alterra/basic-echo-mvc/pkg/util/response"
+	aws_util "github.com/born2ngopi/alterra/basic-echo-mvc/pkg/util/aws"
+	"github.com/born2ngopi/alterra/basic-echo-mvc/pkg/util/str"
 )
 
 type service struct {
@@ -58,6 +66,29 @@ func (s *service) FindByID(ctx context.Context, payload *dto.ByIDRequest) (*mode
 
 func (s *service) Create(ctx context.Context, payload *dto.CreateNewsRequest) (string, error) {
 
+	// upload image if exist
+	var uploader *s3manager.Uploader
+	uploader = aws_util.NewUploader()
+	log.Printf("up %+v\n", uploader)
+
+	log.Println("uploading...")
+	file, err := ioutil.ReadFile("Soal.pdf")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	file_destination := "avatar/"+str.GenerateRandString(10)+".pdf"
+
+	upInput := &s3manager.UploadInput{
+		Bucket:      aws.String(os.Getenv("AWS_BUCKET")), // bucket's name
+		Key:         aws.String(file_destination),        // files destination location
+		Body:        bytes.NewReader(file),               // content of the file
+		ContentType: aws.String("file/pdf"),              // content type
+	}
+	resp, err := uploader.UploadWithContext(context.Background(), upInput)
+	log.Printf("res %+v\n", resp)
+	log.Printf("err %+v\n", err)
+
 	var News = model.News{
 		Title:       payload.Title,
 		Image:       payload.Image,
@@ -65,9 +96,9 @@ func (s *service) Create(ctx context.Context, payload *dto.CreateNewsRequest) (s
 		IsActive:    payload.IsActive,
 	}
 
-	err := s.NewsRepository.Create(ctx, News)
-	if err != nil {
-		return "", res.ErrorBuilder(&res.ErrorConstant.InternalServerError, err)
+	err2 := s.NewsRepository.Create(ctx, News)
+	if err2 != nil {
+		return "", res.ErrorBuilder(&res.ErrorConstant.InternalServerError, err2)
 	}
 
 	return "success", nil
